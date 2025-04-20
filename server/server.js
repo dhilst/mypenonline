335807ca6d1c89b52d0b3e4792e959653841fb2e
Daniel Hilst <danielhilst@gmail.com>
2025-04-19 23:58:36 -0300

    Remove websocket

--- server/server.js
+++ server/server.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const WebSocket = require('ws');
 
 const app = express();
 const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server });
-// 
-// const clients = new Set();
-// 
-// wss.on('connection', (ws) => {
-//   clients.add(ws);
-// 
-//   ws.on('message', (message) => {
-//     for (const client of clients) {
-//       if (client !== ws && client.readyState === WebSocket.OPEN) {
-//         client.send(message);
-//       }
-//     }
-//   });
-// 
-//   ws.on('close', () => {
-//     clients.delete(ws);
-//   });
-// });
 
 // Serve static files from ../client/
 const clientPath = path.join(__dirname, '../client');
